8230e4e1c0f297f6eee1c9e97e441030e0861bfb
Nicolas <90008745+CastelliNicolas@users.noreply.github.com>
2024-05-05 20:13:35 -0300

    parte compra agregada

--- JS/main.js
+++ JS/main.js
@@ -9,3 +9,28 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
   nav.classList.remove("visible");
 });
+
+function mostrarInfo(){
+  let pelicula =  document.getElementById("selection-peli").value;
+  let info = document.getElementById("compraInfo");
+  let portadaImg = document.getElementById("portadaImagen");
+  let titulo = document.getElementById("tituloFilm");
+  switch(pelicula){
+    case "Film 1":
+      titulo.textContent = "Forest Gump";
+      info.style.display = "grid";
+      portadaImg.src = "/img/forest.jpg";
+      break;
+    case "Film 2":
+      info.style.display = "grid";
+      portadaImg.src = "/img/gladiador.jpg";
+      titulo.textContent = "Gladiador";
+      break;
+    case "Film 3":
+      info.style.display = "grid";
+      titulo.textContent = "Rapidos Y Furiosos";
+      portadaImg.src = "/img/rapido.webp";
+      break;
+      
+  }
+}
