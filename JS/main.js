28395fb5497fa4aaf4ccac10e3a34f1d47a330ad
Nicolas <90008745+CastelliNicolas@users.noreply.github.com>
2024-05-06 22:54:48 -0300

    agregado btn dinamico y fix querys

--- JS/main.js
+++ JS/main.js
@@ -10,26 +10,65 @@ cerrar.addEventListener("click", () => {
   nav.classList.remove("visible");
 });
 
+
+// Compra //
+let selectCine = document.getElementById("selection-cine");
+let selectFilm = document.getElementById("selection-peli"); 
+let selectHour = document.getElementById("selection-horario");
+let buttonBuy = document.getElementById("btn-comprar");
+function verificarSelection(){
+  if(selectCine.value && selectFilm.value && selectHour.value){
+    console.log("TRUE");
+    buttonBuy.classList.remove("disable");
+    buttonBuy.setAttribute("href", "sesion.html");
+  };
+};
+selectCine.addEventListener("change", verificarSelection)
+selectFilm.addEventListener("change", verificarSelection)
+selectHour.addEventListener("change", verificarSelection)
+
+
+
 function mostrarInfo(){
   let pelicula =  document.getElementById("selection-peli").value;
   let info = document.getElementById("compraInfo");
   let portadaImg = document.getElementById("portadaImagen");
   let titulo = document.getElementById("tituloFilm");
+  let infoGen = document.getElementById("infoGen");
+  let infoDur = document.getElementById("infoDur");
+  let infoDir = document.getElementById("infoDir");
+  let infoAct = document.getElementById("infoAct");
+  let infoSinop = document.getElementById("sinopFilm");
   switch(pelicula){
     case "Film 1":
       titulo.textContent = "Forest Gump";
       info.style.display = "grid";
       portadaImg.src = "/img/forest.jpg";
+      infoGen.textContent = "Comedia, Drama, Romántico";
+      infoDur.textContent = "2h 20min";
+      infoDir.textContent = "Robert Zemeckis";
+      infoAct.textContent = "Tom Hanks, Gary Sinise, Robin Wright";
+      infoSinop.textContent = "Al tener el coeficiente intelectual de un niño, Forrest Gump siempre ha sido considerado el “tonto” de clase. Bajo las faldas de su madre se siente protegido y junto a su amiga Jenny es feliz, aunque en su propio mundo. Un problema en su columna vertebral no le impide convertirse en un ágil corredor. Ya más mayor, Forrest luchará en la guerra de Vietnam y conocerá al mismísimo presidente de los Estados Unidos. Llegará a ser muy rico, pero para Forrest hay algo que no cambia: el amor de su vida es y será Jenny."
       break;
     case "Film 2":
       info.style.display = "grid";
       portadaImg.src = "/img/gladiador.jpg";
       titulo.textContent = "Gladiador";
+      infoGen.textContent = "Acción, Aventura, Histórico";
+      infoDur.textContent = "2h 35min";
+      infoDir.textContent = "Ridley Scott";
+      infoAct.textContent = "Russell Crowe, Joaquin Phoenix, Connie Nielsen";
+      infoSinop.textContent = "En el año 180 el Imperio Romano controla todo el mundo conocido hasta la fecha. Máximo, interpretado por el ya conocido Russell Crowe, es un General romano muy importante para el Emperador Marco Aurelio, pues sólo él ha conseguido victoria tras victoria, destacando por su valentía, dedicación y lealtad al Imperio. Cómodo, el hijo de Marco Aurelio, está celoso del prestigio de Máximo y del amor que le profesa su padre, así que cuando asume el poder de manera inesperada, ordena el arresto y la ejecución del general. Máximo consigue escapar de sus opresores, pero no puede impedir que asesinen a su familia. Posteriormente, es capturado por un mercader de esclavos y se convierte en gladiador, preparándose para su venganza."
       break;
     case "Film 3":
       info.style.display = "grid";
-      titulo.textContent = "Rapidos Y Furiosos";
+      titulo.textContent = "Rapido Y Furioso";
       portadaImg.src = "/img/rapido.webp";
+      infoGen.textContent = "Acción, Crimen";
+      infoDur.textContent = "1h 47min";
+      infoDir.textContent = "Rob Cohen";
+      infoAct.textContent = "Paul Walker, Vin Diesel, Michelle Rodriguez";
+      infoSinop.textContent = "La vida de Dominic Toretto (Vin Diesel) está marcada día y noche por la velocidad. Durante el día prepara y tunea coches de carreras de alta gama, mejora sus prestaciones y los embellece. Al caer la noche se organizan competiciones ilegales donde muchos candidatos se enfrentan sin contemplaciones bajo la intensa mirada de sus groupies. Dominic Toretto compite en ellas con su propio bólido a 280 kilómetros por hora, consiguiendo numerosas sumas de dinero gracias a sus victorias.\nSin embargo, existe una banda de delincuentes muy misteriosa que roba camiones en marcha desde vehículos deportivos. Este hecho hace que la policía infiltre a uno de sus hombres dentro de las carreras ilegales para investigar a posibles sospechosos. Brian (Paul Walker) es el encargado de infiltrarse en la banda de Toretto, la que figura junto a la de su rival Johnny Tran como principales sospechosos. Rápidamente se ganará la confianza de Toretto, pero todo se complicará cuando aparezca su hermana y se enamora de ella."
       break;
       
   }
